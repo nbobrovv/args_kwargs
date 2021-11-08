07c8f9da03dcb03a0ae72f9334f130f5e9b7accf
nbobrov8 <bobrovn8@yandex.ru>
2021-11-08 13:02:29 +0300

    modificated task

--- tasks/task_3.py
+++ tasks/task_3.py
@@ -2,22 +2,17 @@
 # -*- coding: utf-8 -*-
 
 """
-Найти среднее арифметическое чисел.
+Найти среднее арифметическое чисел и их сумму.
 """
 
 
 def arithmetic(**kwargs):
     x = 0
-    k = 0
     len_of = len(kwargs)
     for numbers in kwargs.values():
         x += float(numbers)
+    print(f"Cумма чисел: {x}")
     sum_of = x / len_of
-
-    for n in kwargs.values():
-        k += float(n)
-    tip = k
-    print(f"Сумма чисел: {tip}")
     print('Среднее арифметическое чисел: ', sum_of)
 
 
