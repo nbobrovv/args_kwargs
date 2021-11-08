32b79a53b0ac7e6f3fd5847b30482bf497252ede
nbobrov8 <bobrovn8@yandex.ru>
2021-11-08 13:16:08 +0300

    Добавлены изменения

--- tasks/task_3.py
+++ tasks/task_3.py
@@ -2,18 +2,23 @@
 # -*- coding: utf-8 -*-
 
 """
-Найти среднее арифметическое чисел и их сумму.
+Найти среднее арифметическое чисел, их сумму
+и наибольшее значение словаря.
 """
 
 
 def arithmetic(**kwargs):
     x = 0
+    max_val = max(kwargs.values())
     len_of = len(kwargs)
     for numbers in kwargs.values():
         x += float(numbers)
     print(f"Cумма чисел: {x}")
     sum_of = x / len_of
     print('Среднее арифметическое чисел: ', sum_of)
+    for k, v in kwargs.items():
+        if v == max_val:
+            print(f"Наибольшая переменная {k} со значением {v}")
 
 
 if __name__ == '__main__':
